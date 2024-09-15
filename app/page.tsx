f3e13debc53b10fe613cade6b5c9d38043c1c03c
Isabella <idavila2021@gmail.com>
2024-09-15 09:44:33 -0600

    feat: links and youtube

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Hero from "@/components/main/Hero";
 import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills"
- 
+import Video from "@/components/main/Video"; 
+
 export default function Home() {
   return (
     <main className="h-full w-full">
-      <div className="flex flex-col  gap-20">
+      <div className="flex flex-col">
         <Hero/>
         <Skills/>
         <Projects/>
+        <Video/>
       </div>
     </main>
   );
