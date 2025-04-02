fb40493eae7fbd2d53f66ee4045d9c6cbf4837ac
Isabella <idavila2021@gmail.com>
2025-04-02 09:40:41 -0600

    chore(layout): move skills to bottom

--- app/page.tsx
+++ app/page.tsx
@@ -16,14 +16,14 @@ export default function Home() {
         <section id="hero">
           <Hero />
         </section>
-        <section id="skills">
-          <Skills />
-        </section>
         <section id="projects">
           <Projects />
         </section>
         <section id="lab-reports">
           <Labs />
+        </section>
+        <section id="skills">
+          <Skills />
         </section>
         {/* <section id="contacts">
           <Contacts/>
